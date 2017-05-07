044f79ea2fee116732ba1815a127a38bd88c7f29
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-07 22:37:55 +0100

    Refactor and add database persistence

---
+++ src/Api.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Api (app) where
+
+import Control.Monad.Except
+import Control.Monad.Reader        (ReaderT, runReaderT)
+import Control.Monad.Reader.Class
+import Data.Int                    (Int64)
+import Database.Persist.Postgresql (Entity (..), fromSqlKey, insert,
+                                    selectFirst, selectList, (==.))
+import Network.Wai                 (Application)
+import Servant
+
+import Config                      (App (..), Config (..))
+import Models
+
+import Api.Train
+
+-- | This is the function we export to run our 'trainAPI'. Given a 'Config', we
+-- return a WAI 'Application' which any WAI compliant server can run.
+trainApp :: Config -> Application
+trainApp cfg = serve (Proxy :: Proxy TrainAPI) (appToServer cfg)
+
+-- | This functions tells Servant how to run the 'App' monad with our 'server'
+-- function.
+appToServer :: Config -> Server TrainAPI
+appToServer cfg = enter (convertApp cfg) trainServer
+
+-- | This function converts our 'App' monad into the @ExceptT ServantErr IO@
+-- monad that Servant's 'enter' function needs in order to run the application.
+-- The ':~>' type is a natural transformation, or, in non-category theory terms,
+-- a function that converts two type constructors without looking at the values
+-- in the types.
+convertApp :: Config -> App :~> ExceptT ServantErr IO
+convertApp cfg = Nat (flip runReaderT cfg . runApp)
+
+type API = TrainAPI
+
+appApi :: Proxy API
+appApi = Proxy
+
+app :: Config -> Application
+app cfg = serve appApi (appToServer cfg)
--- src/Models.hs
+++ src/Models.hs
@@ -1,15 +1,37 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Models where
 
-import Data.Aeson
-import Data.Aeson.TH
+import Control.Monad.Reader
+import Data.Aeson           (FromJSON, ToJSON)
+import Database.Persist.Sql
+import Database.Persist.TH  (mkMigrate, mkPersist, persistLowerCase,
+                             share, sqlSettings)
+import GHC.Generics         (Generic)
 
-data Train = Train
-  { id :: Int
-  , name :: String
-  } deriving (Eq, Show)
+import Config
 
-$(deriveJSON defaultOptions ''Train)
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Train json
+    name String
+    deriving Show
+|]
+
+doMigrations :: SqlPersistT IO ()
+doMigrations = runMigration migrateAll
+
+runDb :: (MonadReader Config m, MonadIO m) => SqlPersistT IO b -> m b
+runDb query = do
+    pool <- asks getPool
+    liftIO $ runSqlPool query pool
