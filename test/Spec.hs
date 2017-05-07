e8e1e489b2c0d2015bb07ba9c6b50dec370984e8
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-07 15:48:20 +0100

    Return a mock list of trains

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
 import Lib (app)
+
 import Test.Hspec
 import Test.Hspec.Wai
 import Test.Hspec.Wai.JSON
@@ -12,9 +13,9 @@ main = hspec spec
 
 spec :: Spec
 spec = with (return app) $ do
-    describe "GET /users" $ do
-        it "responds with 200" $ do
-            get "/users" `shouldRespondWith` 200
-        it "responds with [User]" $ do
-            let users = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"}]"
-            get "/users" `shouldRespondWith` users
+  describe "GET /trains" $ do
+    it "responds with 200" $ do
+      get "/trains" `shouldRespondWith` 200
+    it "responds with [Train]" $ do
+      let trains = "[{\"id\":1,\"name\":\"Alpha Train\"},{\"id\":2,\"name\":\"Beta Train\"}]"
+      get "/trains" `shouldRespondWith` trains
