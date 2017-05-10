1ba9b169c3d37d1439a5d719991cc6cec6f006cc
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2017-05-10 20:18:57 +0100

    Retrieve a specific train by name from database

--- src/Api/Train.hs
+++ src/Api/Train.hs
@@ -3,17 +3,26 @@
 
 module Api.Train where
 
+import Database.Persist.Postgresql (Entity (..), selectList, selectFirst, (==.))
+import Servant
+
 import Config (App (..), Config (..))
 import Models
 
-import Database.Persist.Postgresql (Entity (..), selectList)
-import Servant
-
-type TrainAPI = "trains" :> Get '[JSON] [Entity Train]
+type TrainAPI =
+       "trains" :> Get '[JSON] [Entity Train]
+  :<|> "trains" :> Capture "name" String :> Get '[JSON] (Entity Train)
 
 -- | The server that runs the TrainAPI
 trainServer :: ServerT TrainAPI App
-trainServer = allTrains
+trainServer = allTrains :<|> getTrain
 
 allTrains :: App [Entity Train]
 allTrains = runDb (selectList [] [])
+
+getTrain :: String -> App (Entity Train)
+getTrain str = do
+  maybeTrain <- runDb (selectFirst [TrainName ==. str] [])
+  case maybeTrain of
+    Just train -> return train
+    Nothing    -> throwError err404
