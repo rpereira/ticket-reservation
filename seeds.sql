cd0493f1073c8db66c3152ce3dd0013fdeaa3404
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-12 10:28:13 +0100

    Seed station table

--- seeds.sql
+++ seeds.sql
@@ -3,3 +3,5 @@ TRUNCATE train, station;
 INSERT INTO train VALUES (1, 'Alpha Train');
 INSERT INTO train VALUES (2, 'Beta Train');
 INSERT INTO train VALUES (3, 'Zeta Train');
+
+COPY station (name) FROM '/Users/rap/code/thesis/TicketReservation/metadata/stations.txt';
