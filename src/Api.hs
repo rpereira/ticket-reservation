929a986674adbdd92f2b8cdf5dd85bae808ce415
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-12 08:29:04 +0100

    Better organize file

--- src/Api.hs
+++ src/Api.hs
@@ -14,23 +14,25 @@ import Models
 
 import Api.Train
 
--- | This functions tells Servant how to run the 'App' monad with our 'server'
--- function.
-appToServer :: Config -> Server TrainAPI
-appToServer cfg = enter (convertApp cfg) trainServer
-
--- | This function converts our 'App' monad into the @ExceptT ServantErr IO@
--- monad that Servant's 'enter' function needs in order to run the application.
--- The ':~>' type is a natural transformation, or, in non-category theory terms,
--- a function that converts two type constructors without looking at the values
--- in the types.
-convertApp :: Config -> App :~> ExceptT ServantErr IO
-convertApp cfg = Nat (flip runReaderT cfg . runApp)
-
 type API = TrainAPI
+
+-- | Combinate all endpoints to be served.
+server :: ServerT API App
+server = trainServer
 
 appApi :: Proxy API
 appApi = Proxy
+
+-- | Converts 'App' monad into the @ExceptT ServantErr IO@ monad that Servant's
+-- 'enter' function needs in order to run the application.  The ':~>' type is a
+-- natural transformation, or, in non-category theory terms, a function that
+-- converts two type constructors without looking at the values in the types.
+convertApp :: Config -> App :~> ExceptT ServantErr IO
+convertApp cfg = Nat (flip runReaderT cfg . runApp)
+
+-- | Tell Servant how to run the 'App' monad with the 'server' function.
+appToServer :: Config -> Server API
+appToServer cfg = enter (convertApp cfg) server
 
 app :: Config -> Application
 app cfg = serve appApi (appToServer cfg)
