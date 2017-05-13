ba435ff331f98452dec6fcf4b6e589faf5def88f
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-13 11:27:54 +0100

    Add metadata containing CRS codes for each station name

--- seeds.sql
+++ seeds.sql
@@ -4,4 +4,6 @@ INSERT INTO train VALUES (1, 'Alpha Train');
 INSERT INTO train VALUES (2, 'Beta Train');
 INSERT INTO train VALUES (3, 'Zeta Train');
 
-COPY station (name) FROM '/Users/rap/code/thesis/TicketReservation/metadata/stations.txt';
+COPY station (name, code)
+  FROM '/Users/rap/code/thesis/TicketReservation/metadata/station_codes.csv'
+  DELIMITER ',' CSV;
