10329ab573d2d4960bf44660f011c76dd5adad1d
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-14 12:42:37 +0100

    Fetch timetables for each station

--- scripts/build_dataset.rb
+++ scripts/build_dataset.rb
@@ -2,7 +2,12 @@ require 'net/http'
 require 'csv'
 require 'json'
 
-STATIONS_URL = 'https://transportapi.com/v3/uk/train/stations/bbox.json?app_id=221cce2f&app_key=d209929236fc97196775650c2bdb639e&maxlat=51.2868&maxlon=0.3340&minlat=51.6923&minlon=-0.5103'
+BASE_URL       = 'https://transportapi.com/v3/uk/train'
+STATIONS_URL   = "#{BASE_URL}/stations/bbox.json?app_id=221cce2f&app_key=d209929236fc97196775650c2bdb639e&maxlat=51.2868&maxlon=0.3340&minlat=51.6923&minlon=-0.5103"
+TIME_TABLE_URL = "#{BASE_URL}/station/$0/2017-05-15/06:00/timetable.json?app_id=221cce2f&app_key=d209929236fc97196775650c2bdb639e&to_offset=PT10:00:00&train_status=passenger&type=arrival,departure,pass"
+
+STATIONS_FILE_PATH  = 'metadata/stations.csv'
+SCHEDULES_FILE_PATH = 'metadata/schedules.csv'
 
 def json_to_csv(data)
   column_names = data.first.keys
@@ -15,8 +20,16 @@ def json_to_csv(data)
   csv_string
 end
 
-def write_to_file(data, path)
-  File.open(path, 'w') do |f|
+def csv_to_json(fpath)
+  rows = []
+  CSV.foreach(fpath, headers: true) do |row|
+    rows.push row['crs_code']
+  end
+  rows
+end
+
+def write_to_file(data, fpath)
+  File.open(fpath, 'w') do |f|
     f.write(data)
   end
 end
@@ -56,9 +69,38 @@ def parse_stations(data)
   stations
 end
 
+def fetch_schedules
+  puts "\n==> Fetching time tables"
+  schedules = []
+  crs_codes = csv_to_json(STATIONS_FILE_PATH)  # better API would be pass crs_code key as arg
+  crs_codes.each do |code|
+    schedules.concat fetch_schedule_for_station(code)
+  end
+  schedules
+end
+
+def fetch_schedule_for_station(crs_code)
+  print "#{crs_code} "
+  departures = []
+  url = TIME_TABLE_URL.clone.sub! '$0', crs_code
+  response = get_as_json(url)
+  departures.concat parse_departures(response)
+end
+
+def parse_departures(data)
+  departures = []
+  data['updates']['all'].each do |x|
+    next unless x['mode'] == 'train'
+    departures.push x
+  end
+end
+
 def main
   stations = fetch_stations
-  write_to_file(json_to_csv(stations), 'metadata/stations.csv')
+  write_to_file(json_to_csv(stations), STATIONS_FILE_PATH)
+
+  schedules = fetch_schedules
+  write_to_file(json_to_csv(schedules), SCHEDULES_FILE_PATH)
 end
 
 main
