6838178b5628da48b651e39191dbff30f62ad1e0
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-14 13:08:08 +0100

    Update entity Station

---
+++ metadata/README.md
@@ -0,0 +1,10 @@
+# Fields Description
+
+## Station
+
+**`name`:** the name of the station
+
+**`crs_code`:** station unique identifier
+
+**`tiplic_code`:** used by train planners to identify what time trains should
+arrive at, depart or pass at a particular point. Limited to seven characters.
--- seeds.sql
+++ seeds.sql
@@ -4,6 +4,6 @@ INSERT INTO train VALUES (1, 'Alpha Train');
 INSERT INTO train VALUES (2, 'Beta Train');
 INSERT INTO train VALUES (3, 'Zeta Train');
 
-COPY station (name, code)
-  FROM '/Users/rap/code/thesis/TicketReservation/metadata/station_codes.csv'
-  DELIMITER ',' CSV;
+COPY station (name, crs_code, tiplic_code)
+  FROM '/Users/rap/code/thesis/TicketReservation/metadata/stations.csv'
+  DELIMITER ',' CSV HEADER;
