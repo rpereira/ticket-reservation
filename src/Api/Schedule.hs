c927cf042306e4584cec876ab6046498d64008f5
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2017-05-15 23:25:01 +0100

    Get all entries in schedule entity for specified station

--- src/Api/Schedule.hs
+++ src/Api/Schedule.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Api.Schedule where
 
+import Data.Maybe                  (Maybe, listToMaybe)
 import Data.Text                   (Text)
 import Database.Persist.Postgresql (Entity (..), selectList, selectFirst, (==.))
 import Servant
@@ -10,11 +12,20 @@ import Servant
 import Config (App (..), Config (..))
 import Models
 
-type ScheduleAPI = "schedules" :> Get '[JSON] [Entity Schedule]
+type ScheduleAPI =
+       "schedules" :> Get '[JSON] [Entity Schedule]
+  :<|> "schedules" :> Capture "crs" Text :> Get '[JSON] [Entity Schedule]
 
 -- | The server that runs the ScheduleAPI
 scheduleServer :: ServerT ScheduleAPI App
-scheduleServer = allSchedules
+scheduleServer = allSchedules :<|> getScheduleForStation
 
 allSchedules :: App [Entity Schedule]
 allSchedules = runDb (selectList [] [])
+
+getScheduleForStation :: Text -> App [Entity Schedule]
+getScheduleForStation crsCode = runDb $ do
+  station <- selectFirst [StationCrsCode ==. crsCode] []
+  case station of
+    Nothing -> return []
+    Just s -> selectList [ScheduleStationId ==. entityKey s] []
