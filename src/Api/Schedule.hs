b23c0272f3e4650751bce4879dd699a3b78c9da9
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2017-05-19 17:31:26 +0100

    Update example for train entity and fix endpoint

--- src/Api/Schedule.hs
+++ src/Api/Schedule.hs
@@ -6,19 +6,28 @@ module Api.Schedule where
 
 import Data.Maybe                  (Maybe, listToMaybe)
 import Data.Text                   (Text)
-import Database.Persist.Postgresql (Entity (..), selectList, selectFirst, (==.))
+import Database.Persist.Postgresql (Entity (..), selectList, selectFirst, (==.), (>.))
 import Servant
+import Data.Time.Clock.POSIX
+import Data.Time            (UTCTime)
+-- import Data.Time.Clock.UTC
 
 import Config (App (..), Config (..))
 import Models
 
+epochToUTC :: Integer -> UTCTime
+epochToUTC t = posixSecondsToUTCTime $ fromInteger t / 1000
+
 type ScheduleAPI =
        "schedules" :> Get '[JSON] [Entity Schedule]
-  :<|> "schedules" :> Capture "crs" Text :> Get '[JSON] [Entity Schedule]
+  :<|> "schedules" :> Capture "crs" Text
+                   :> Get '[JSON] [Entity Schedule]
+  :<|> "schedules" :> Capture "time" UTCTime
+                   :> Get '[JSON] [Entity Schedule]
 
 -- | The server that runs the ScheduleAPI
 scheduleServer :: ServerT ScheduleAPI App
-scheduleServer = allSchedules :<|> getScheduleForStation
+scheduleServer = allSchedules :<|> getScheduleForStation :<|> getScheduleForStation'
 
 allSchedules :: App [Entity Schedule]
 allSchedules = runDb (selectList [] [])
@@ -29,3 +38,11 @@ getScheduleForStation crsCode = runDb $ do
   case station of
     Nothing -> return []
     Just s -> selectList [ScheduleStationId ==. entityKey s] []
+
+getScheduleForStation' :: UTCTime -> App [Entity Schedule]
+getScheduleForStation' time = runDb $ do
+  station <- selectFirst [StationCrsCode ==. "LBG"] []
+  case station of
+    Nothing -> return []
+    Just s -> selectList [ScheduleStationId ==. entityKey s,
+                          ScheduleDepartureTime >. time] []
