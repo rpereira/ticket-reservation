ac5fa7b94b99434148407118353c3c92b0f69532
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2017-05-23 11:23:02 +0100

    Add pagination support to GET /schedules

--- src/Api/Schedule.hs
+++ src/Api/Schedule.hs
@@ -4,19 +4,30 @@
 
 module Api.Schedule where
 
-import Data.Maybe                  (Maybe, listToMaybe)
+import Data.Maybe                  (Maybe, fromMaybe)
 import Data.Text                   (Text)
 import Database.Persist.Postgresql (Entity (..), selectList)
+import Database.Persist.Types      (SelectOpt (..))
 import Servant
 
 import Config                      (App (..), Config (..))
 import Models
 
-type ScheduleAPI = "schedules" :> Get '[JSON] [Entity Schedule]
+-- | Always limit response to this many results.
+resultsPerPage = 30
+
+type ScheduleAPI = "schedules" :> QueryParam "page" Int
+                               :> Get '[JSON] [Entity Schedule]
 
 -- | The server that runs the ScheduleAPI
 scheduleServer :: ServerT ScheduleAPI App
 scheduleServer = allSchedules
 
-allSchedules :: App [Entity Schedule]
-allSchedules = runDb (selectList [] [])
+allSchedules :: Maybe Int -> App [Entity Schedule]
+allSchedules pageNumber = do
+  let page = fromMaybe 1 pageNumber
+  runDb (selectList
+    []
+    [ LimitTo resultsPerPage,
+      OffsetBy $ (page - 1) * resultsPerPage
+    ])
